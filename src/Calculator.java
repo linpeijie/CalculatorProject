67a413cfc6dd7f47f2952064565216f8e2b15b9f
linpeijie <lpj498840744@qq.com>
2018-03-14 16:03:58 +0800

    设置窗体风格

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,15 +2,10 @@ import javax.swing.UIManager;
 
 public class Calculator {	
     public static void main(String[] args) {       
-    		try {
-    			UIManager.setLookAndFeel("com.sun.java.swing.plaf.mac.MacLookAndFeel");
-    		} catch(Exception e) {
-    			e.printStackTrace();
-    		}
     		
     	 	Windows windows = new Windows();
-            //运行窗体
-            
+        
+    	 	//运行窗体
         windows.setVisible(true);
    
     }   
